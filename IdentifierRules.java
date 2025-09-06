bca65c0bc2aea26c4ae82c3bb7f1ffb2ff1afa57
liao2404 <cysonliew23@gmail.com>
2025-09-06 14:31:46 +0800

    IdentifierRules.java v.1.1

--- IdentifierRules.java
+++ IdentifierRules.java
@@ -3,8 +3,8 @@
   Program Title : Demonstrating Identifier Rules in Java
   File Name: "IdentifierRules.java"
   
-  Programmer : Sathish Sarma
-  Contact Details : sathish.sarma@newinti.edu.my
+  Programmer : Liao
+  Contact Details : j24041998@student.newinti.edu.my
   Date : 04 September 2025
   
  **********************************************************************/
@@ -18,38 +18,46 @@ class SwitchStatus{
 }
 
 public class IdentifierRules {
-	
-    //public static void 1start() { 
+    
+    /******************
+    Rule for Naming an IdentifierRules
+    r1 - Identifier cannot begin with a Number
+    r2 - Identifier names cannot contain whitespaces or special Character
+    r3 - Reserved words cannot be used as identifier
+    r4 - Identifier names are case-sensitive
+    *******************/
+    
+    //public static void 1start() { (r1)
     public static void start() {
 		System.out.println("Static Method Output");
 	}
 
     public static void main(String[] args) {
 
-        //int 2ndPlace = 2;
+        //int 2ndPlace = 2;(r1)
         int SecondPlace = 2;
         
-        //int first name = 10;
+        //int first name = 10; (r2)
         int first_name = 10;
 
-        //int price# = 99;
+        //int price# = 99; (r2)
         int price = 99;
-		//int user-name = 25;
+		//int user-name = 25; (r2)
 		int user_name = 25;
 
-		//int class = 1;
+		//int class = 1; (r3)
 		int _class = 1;
-		//int double = 3;
+		//int double = 3; (r3)
 		int _double = 3;
 
         int Score = 10;
-        //System.out.println(score);
+        //System.out.println(score); (r4)
         System.out.println(Score);
 		
 		System.out.println("This program is executed after all errors are rectified.");
 
 
-        //1start();
+        //1start(); (r1)
         start();
     }
 

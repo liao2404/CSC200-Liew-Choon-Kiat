7f2487f111feaf36cd0c27e85f57463a9dbaae89
liao2404 <cysonliew23@gmail.com>
2025-09-09 15:23:01 +0800

    Create OutputSpecialCharacters.java

---
+++ OutputSpecialCharacters.java
@@ -0,0 +1,52 @@
+/**********************************************************************
+  
+  Program Title  : Display Special Characters in Java
+  File Name      : "OutputSpecialCharacters.java"
+  
+  Programmer     : Sathish Sarma
+  Contact Details: sathish.sarma@newinti.edu.my
+  Date           : 25 August 2025
+  
+ **********************************************************************/
+
+public class OutputSpecialCharacters {
+    public static void main(String[] args) {
+		
+		/*****
+		Update the code below to ensure all special characters are placed accordingly and correctly.
+		The string XYZ is used as the placeholder for the special characters.
+		Replace all println with print and use suitable special character as replacement.
+		*****/
+
+        System.out.print("Demonstrating Special Characters in Java\n");
+        System.out.print("---------------------------------------\n");
+
+        // New line
+        System.out.print("New line example:\nHello\nWorld\n");
+
+        // Tab
+        System.out.print("Tab example:\tJava\tProgramming\t\n");
+		System.out.print("\n");
+
+        // Backslash
+        System.out.print("Backslash: \\\n");
+
+        // Double quote
+        System.out.print("Double quote: \"Hello\"\n");
+
+        // Single quote
+        System.out.print("Single quote: \'A\'\n");
+
+        // Carriage return
+        System.out.print("Carriage return example:\n");
+        System.out.print("Line \rline");
+		System.out.print("\n");
+
+        // Backspace
+        System.out.println("Backspace example: ABC\bD \n");
+
+        // Form feed (note: effect depends on system/console)
+        System.out.print("Form feed example: First page\fSecond page\n");
+
+    }
+}

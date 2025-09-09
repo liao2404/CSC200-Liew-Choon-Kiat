0055042ffa1d96872d3d675daf47d424041d906d
liao2404 <cysonliew23@gmail.com>
2025-09-09 16:35:31 +0800

    Create WelcomingScreen.java

---
+++ WelcomingScreen.java
@@ -0,0 +1,41 @@
+/**********************************************************************
+Program Title : Decorative Welcoming Screen
+File Name     : "WelcomingScreen.java"
+Programmer    : Liao
+Contact Details : j24041998@student.newinti.edu.my
+Date          : 9 September 2025
+**********************************************************************/
+public class WelcomingScreen
+{
+    public static void main(String[] args)
+    {
+    // 
+    System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+    System.out.println("|                                                                                   |");
+    System.out.println("|                               @ Lab Exercise 1 @                                  |");
+    System.out.println("|                                                                                   |");
+    System.out.println("|           ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~          |");
+    System.out.println("|           ^ Student Name : Liew Choon Kiat                             ^          |");
+    System.out.println("|           ^ Student ID : J24041998                                     ^          |");
+    System.out.println("|           ^ University Name : INTI International College Subang (IICS) ^          |");
+    System.out.println("|           ^ Programme : AUP                                            ^          |");
+    System.out.println("|           ^ Course Code : CSC200                                       ^          |");
+    System.out.println("|           ^ Course Title : Object-Oriented Programming in JAVA I       ^          |");
+    System.out.println("|           ^ Task ID : LAB01_CSC200_CA3(5)_CLO3                         ^          |");
+    System.out.println("|           ^ Task Title : Decorative Welcoming Screen                   ^          |");
+    System.out.println("|           ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~          |");
+    System.out.println("|                                                                                   |");
+    System.out.println("|                                                                                   |");
+    System.out.println("|                   LEARN what you're WEAK at,                                      |");
+    System.out.println("|                            and FORGE it into your STRENGTH.                       |");
+    System.out.println("|                                                                                   |");
+    System.out.println("|           What is life?                                                           |");
+    System.out.println("|           ```````````````                                                         |");
+    System.out.println("|           int experience = 0;                                                     |");
+    System.out.println("|           while (alive == true) {                                                 |");
+    System.out.println("|               experience ++;                                                      |");
+    System.out.println("|           }                                                                       |");
+    System.out.println("|                                                                                   |");
+    System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+    }
+}
